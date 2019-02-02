7ee6c7276ede5645aca6f540bf01eea9759d1163
dT <dTNL.Homey@gmail.com>
2019-02-02 09:58:45 +0100

    Make Lisa compatible with `thermostat`

--- drivers/lisa/driver.js
+++ drivers/lisa/driver.js
@@ -4,9 +4,11 @@ const Homey = require('homey');
 const PlugwiseAdamDriver = require('../../lib/PlugwiseAdamDriver');
 
 module.exports = class PlugwiseAdamLisaDriver extends PlugwiseAdamDriver {
-	
+
 	onPairFilterAppliance({ appliance }) {
-  	return appliance.type === 'zone_thermostat';
+		if (appliance.type === 'zone_thermostat') return true;
+		if (appliance.type === 'thermostat') return true;
+		return false;
 	}
-	
+
 }
